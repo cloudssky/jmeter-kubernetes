c020cf3f55f8c14b1fab052d7d8fe651c14f8144
Arash Kaffamanesh <arash.kaffamanesh.ext@dkv-mobility.com>
2019-04-04 22:50:26 +0200

    support for pepper box kafka jmeter 5.x plugin

--- dockerimages.sh
+++ dockerimages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-docker build --tag="kubernautslabs/jmeter-base:latest" -f Dockerfile-base .
-docker build --tag="kubernautslabs/jmeter-master:latest" -f Dockerfile-master .
-docker build --tag="kubernautslabs/jmeter-slave:latest" -f Dockerfile-slave .
-docker build --tag="kubernautslabs/jmeter-reporter" -f Dockerfile-reporter .
+docker build --tag="cloudssky/jmeter-base:latest" -f Dockerfile-base .
+docker build --tag="cloudssky/jmeter-master:latest" -f Dockerfile-master .
+docker build --tag="cloudssky/jmeter-slave:latest" -f Dockerfile-slave .
+docker build --tag="cloudssky/jmeter-reporter" -f Dockerfile-reporter .
